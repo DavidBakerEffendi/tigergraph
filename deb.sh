1309d71b05945a32aa7dbcf3495ab07c8150ee88
David Baker Effendi <davidbakereffendi@hotmail.com>
2020-05-04 17:59:27 +0200

    :bento: Reordered files

--- deb.sh
+++ deb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-source ./config.conf
 
-docker build -t ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION}-${DEB_VERSION} -f ./Dockerfile-${DEB_VERSION} . || exit 1
+source ./acc.conf
+source ./debian-jessie/config.conf
+
+docker build -t ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION}-${DEB_VERSION} ./debian-${DEB_VERSION} || exit 1
 read -r -p "Would you like to push the image? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]];
 then
