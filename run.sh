5dca1d5fc801f453b44061cb90fea8c8dcd06b9c
David Baker Effendi <davidbakereffendi@hotmail.com>
2020-07-11 17:42:49 +0200

    :arrow_up: Made appropriate changes for 3.0.0

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 source ./resources/config.conf
 
-docker build -t ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION}-${DEB_VERSION} . || exit 1
+docker build -t ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION} . || exit 1
 read -r -p "Would you like to push the image? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]];
 then
-    docker push ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION}-${DEB_VERSION}
+    docker push ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION}
 fi
